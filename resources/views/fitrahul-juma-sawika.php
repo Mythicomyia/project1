017da4a89da5685c0412bb6f2f2bc16cb7daf6e4
mythicomyia <fitrahuljumas@gmail.com>
2024-09-30 14:33:16 +0700

    tugas1

---
+++ resources/views/fitrahul-juma-sawika.php
@@ -0,0 +1,1 @@
+fitrahul juma sawika
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/nama', function () {
+    return "Nama saya adalah Fitrahul Juma Sawika";
+});
+
+Route::get('/nim', function () {
+    return "NIM saya adalah 2155201073";
+});
+
+Route::get('/{umur}', function ($umur) {
+    // Redirect ke URL dengan format yang diminta
+    return redirect("/$umur/umur-$umur-tahun");
+});
+
+Route::get('/{umur}/umur-{age}-tahun', function ($umur, $age) {
+    return "Selamat datang, Anda berusia $age tahun!";
+});
